d281d56126472f3109ba40555b70c97ff3cb4976
WowMuchDoge <97766806+WowMuchDoge@users.noreply.github.com>
2023-06-30 10:08:17 -0500

    Add matrix multiplication

--- src/fallingsand/SandLab.java
+++ src/fallingsand/SandLab.java
@@ -33,12 +33,24 @@ public class SandLab
         display = new SandDisplay("Falling Sand", numRows, numCols, names);
         grid = new int[numRows][numCols];
     }
-    
+
     public void buttonMaker(int[][] arr, int button) {
         for (int i = 0; i < arr.length; i++) {
             grid[arr[i][1]][arr[i][0]] = button;
         }
     }
+    
+    public int[] matrixMultiply(int[][] fbfMatrix, int[] fboMatrix) {
+        int[] outputMatrix = {1, 1, 1, 1};
+        outputMatrix[0] = fbfMatrix[0][0] * fboMatrix[0] + fbfMatrix[1][0] * fboMatrix[1] + fbfMatrix[2][0] * fboMatrix[2] + fbfMatrix[3][0] * fboMatrix[3];
+        outputMatrix[1] = fbfMatrix[0][1] * fboMatrix[0] + fbfMatrix[1][1] * fboMatrix[1] + fbfMatrix[2][1] * fboMatrix[2] + fbfMatrix[3][1] * fboMatrix[3];
+        outputMatrix[2] = fbfMatrix[0][2] * fboMatrix[0] + fbfMatrix[1][2] * fboMatrix[1] + fbfMatrix[2][2] * fboMatrix[2] + fbfMatrix[3][2] * fboMatrix[3];
+        outputMatrix[3] = fbfMatrix[0][3] * fboMatrix[0] + fbfMatrix[1][3] * fboMatrix[1] + fbfMatrix[2][3] * fboMatrix[2] + fbfMatrix[3][3] * fboMatrix[3];
+        
+        return outputMatrix;
+    }
+    
+    
     
     int[][] button1 = {{25, 110}, {24, 110}, {24, 109}, {24, 108}, {23, 110}};
     int[][] button2 = {{35, 110}, {36, 110}, {37, 110}, {35, 109}, {35, 111}};    
@@ -116,13 +128,23 @@ public class SandLab
         buttonMaker(button2, BUTTON2);
         buttonMaker(button3, BUTTON3);
         buttonMaker(box, BOX);
+        
+        int[][] matrixOne = {{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}, {13, 14, 15, 16}};
+        int[] matrixTwo = {1, 2, 3, 4};
+        int[] multipliedMatrix = matrixMultiply(matrixOne, matrixTwo);
+        
+        for (int i = 0; i < multipliedMatrix.length; i++) {
+            System.out.print(multipliedMatrix[i]);
+            System.out.print(" ");
+        }
+        System.out.println(" ");
     }
 
     //called repeatedly.
     //causes one random particle to maybe do something.
     public void step()
     {
-
+        
     }
 
     //do not modify
@@ -147,4 +169,6 @@ public class SandLab
             
         }
     }
+
+        
 }
