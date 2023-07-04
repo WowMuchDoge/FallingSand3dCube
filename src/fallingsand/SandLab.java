6d5200aa1e990394c2691e432b10b062443340d2
WowMuchDoge <nolthwaits@esko.k12.mn.us>
2023-07-04 11:07:38 -0500

    Spinny cube

--- src/fallingsand/SandLab.java
+++ src/fallingsand/SandLab.java
@@ -8,17 +8,7 @@ public class SandLab
     //add constants for particle types here
     public static final int EMPTY = 0;
     public static final int METAL = 1;
-    public static final int BUTTON = 2;
-    public static final int BUTTON2 = 3;
-    public static final int BUTTON3 = 4;
-    public static final int BOX = 5;
-    public static final int BUTTON4 = 6;
-    
-    private double cameraX;     // X position of the camera
-    private double cameraY;     // Y position of the camera
-    private double cameraZ;     // Z position of the camera
-    private double cameraYaw;   // Yaw angle of the camera (rotation around the Y axis)
-    private double cameraPitch; // Pitch angle of the camera (rotation around the X axis)
+    public static final int POINT = 2;
 
 
     //do not add any more fields
@@ -42,82 +32,72 @@ public class SandLab
         display = new SandDisplay("Falling Sand", numRows, numCols, names);
         grid = new int[numRows][numCols];
     }
-
-    public void buttonMaker(int[][] arr, int button) {
+    
+    public void pointMaker(float[][] arr, int button) {
+        if (arr[0][1] < ROWS - 1 && arr[0][1] > 0 && arr[0][0] < COLUMNS - 1 && arr[0][0] > 0) {
         for (int i = 0; i < arr.length; i++) {
-            grid[arr[i][1]][arr[i][0]] = button;
-        }
-    }
-    
-    public float[][] perspectiveMatrix(float FOV, float width, float height, float zFar, float zNear) {
-        float aspectRatio = height / width;
-        float[][] returnMatrix = new float[4][4];
-        returnMatrix[0][0] = (float) (aspectRatio * (1/Math.tan(FOV / 2)));
-        returnMatrix[1][1] = (float) (1/Math.tan(FOV / 2));
-        returnMatrix[2][2] = (float) zFar / (zFar - zNear);
-        returnMatrix[2][3] = (float) (-zFar * zNear) / (zFar - zNear);
-        returnMatrix[3][2] = (float) 1.0;
-        
-        return returnMatrix;
-    }
-    
-    public float[] matrixMultiply(float[][] fbfMatrix, float[] fboMatrix) {
-        float[] outputVector = new float[4];
-        outputVector[0] = fboMatrix[0] * fbfMatrix[0][0] + fboMatrix[1] * fbfMatrix[0][1] + fboMatrix[2] * fbfMatrix[0][2] + fboMatrix[3] * fbfMatrix[0][3];
-        outputVector[1] = fboMatrix[0] * fbfMatrix[1][0] + fboMatrix[1] * fbfMatrix[1][1] + fboMatrix[2] * fbfMatrix[1][2] + fboMatrix[3] * fbfMatrix[1][3];
-        outputVector[2] = fboMatrix[0] * fbfMatrix[2][0] + fboMatrix[1] * fbfMatrix[2][1] + fboMatrix[2] * fbfMatrix[2][2] + fboMatrix[3] * fbfMatrix[2][3];
-        outputVector[3] = fboMatrix[0] * fbfMatrix[3][0] + fboMatrix[1] * fbfMatrix[3][1] + fboMatrix[2] * fbfMatrix[3][2] + fboMatrix[3] * fbfMatrix[3][3];
-        
-        return outputVector;
-    }
-    
-    
-    
-    
-    int[][] button1 = {{25, 105}, {24, 105}, {24, 104}, {24, 103}, {23, 105}};
-    int[][] button2 = {{35, 110}, {36, 110}, {37, 110}, {35, 109}, {35, 111}};    
-    int[][] button3 = {{13, 110}, {12, 110}, {11, 110}, {13, 109}, {13, 111}};
-    int[][] button4 = {{25, 115}, {24, 115}, {24, 116}, {24, 117}, {23, 115}};
-    
-    int[][] box = {{50, 50}, {50, 51}, {51, 50}, {51, 51}};
-    
-    //called when the user clicks on a location using the given tool
-    private void locationClicked(int row, int col, int tool, int j)
-            
-    {
-        if (grid[row][col] == BUTTON && j % 10 == 0 && box[0][1] > 0) {
-            System.out.println("Clicked Button");
-            for (int i = 0; i < box.length; i++) {
-                grid[box[i][1]][box[i][0]] = EMPTY;
-                box[i][1] -= 1;
-            }
-        }
-        if (grid[row][col] == BUTTON2 && j % 10 == 0 && box[0][0] < COLUMNS - 2) {
-            System.out.println("Clicked Button");
-            for (int i = 0; i < box.length; i++) {
-                grid[box[i][1]][box[i][0]] = EMPTY;
-                box[i][0] += 1;
-                System.out.println(box[i][0]);
-            }
-        }
-        if (grid[row][col] == BUTTON3 && j % 10 == 0 && box[2][0] > 1) {
-            System.out.println("Clicked Button");
-            for (int i = 0; i < box.length; i++) {
-                grid[box[i][1]][box[i][0]] = EMPTY;
-                box[i][0] -= 1;
-                System.out.println(box[i][0]);
-            }
-        }
-        if (grid[row][col] == BUTTON4 && j % 10 == 0 && box[3][1] < ROWS - 1) {
-            System.out.println("Clicked Button");
-            for (int i = 0; i < box.length; i++) {
-                grid[box[i][1]][box[i][0]] = EMPTY;
-                box[i][1] += 1;
-            }
+            grid[(int)arr[i][1]][(int)arr[i][0]] = POINT;
         }
     };
+        
+    }
     
+    public float[] matrixVectorMultiply(float[][] matrix, float[] vector) {
+        float[] result = new float[matrix.length];
+            for (int i = 0; i < matrix.length; i++) {
+                float sum = 0;
+                for (int j = 0; j < vector.length; j++) {
+                    sum += matrix[i][j] * vector[j];
+                }
+                result[i] = sum;
+            }
+            return result;
+    };
     
+    public float[][] matrixMatrixMultiply(float[][] matrixA, float[][] matrixB) {
+        float[][] result = new float[matrixA.length][matrixB.length];
+        for (int i = 0; i < matrixA.length; i++) {
+            for (int j = 0; j < matrixA.length; j++) {
+                for (int k = 0; k < matrixB.length; k++) {
+                    result[i][j] += matrixA[i][k] * matrixB[k][j];
+                }
+            }
+        }
+        return result;
+    };
+    
+    //called when the user clicks on a location using the given tool
+    private void locationClicked(int row, int col, int tool, int j) {
+        
+    };
+    
+    private void drawLine(int x1, int y1, int x2, int y2) {
+        int dx = Math.abs(x2 - x1);
+        int dy = Math.abs(y2 - y1);
+        int sx = x1 < x2 ? 1 : -1;
+        int sy = y1 < y2 ? 1 : -1;
+        int err = dx - dy;
+
+        while (true) {
+            float[][] point = {{x1, y1}};
+            pointMaker(point, POINT);
+
+            if (x1 == x2 && y1 == y2)
+                break;
+
+            int err2 = 2 * err;
+
+            if (err2 > -dy) {
+                err -= dy;
+                x1 += sx;
+            }
+
+            if (err2 < dx) {
+                err += dx;
+                y1 += sy;
+            }
+        }
+    }
     
 
     //copies each element of grid into the display
@@ -130,45 +110,98 @@ public class SandLab
         {
             for (int column = 0; column < COLUMNS; column++)
             {
-                if (grid[row][column] == EMPTY)
-                {
+                if (grid[row][column] == POINT) {
+                    display.setColor(row, column, metal);
+                }
+                if (grid[row][column] == EMPTY){
                     display.setColor(row, column, empty);
-                }
-                if (grid[row][column] == METAL)
-                {
-                    display.setColor(row, column, metal);
-                }
-                if (grid[row][column] == BUTTON)
-                {
-                    display.setColor(row, column, metal);
-                }
-                if (grid[row][column] == BUTTON2)
-                {
-                    display.setColor(row, column, metal);
-                }
-                if (grid[row][column] == BUTTON3)
-                {
-                    display.setColor(row, column, metal);
-                }
-                if (grid[row][column] == BUTTON4)
-                {
-                    display.setColor(row, column, metal);
-                }
-                if (grid[row][column] == BOX)
-                {
-                    display.setColor(row, column, metal);
                 }
             }
         }
-        buttonMaker(button1, BUTTON);
-        buttonMaker(button2, BUTTON2);
-        buttonMaker(button3, BUTTON3);
-        buttonMaker(button4, BUTTON4);
-        buttonMaker(box, BOX);
         
-        int[][] matrixOne = {{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}, {13, 14, 15, 16}};
-        int[] matrixTwo = {1, 2, 3, 4};
 
+    }
+    
+    float[][] other = {
+        {20, 40, 0},
+       {20, 20, 0},
+       
+       {20, 20, 20},
+       {20, 40, 20},
+    };
+    
+    float[][] points = {
+        {0, 20, 20},
+        {20, 20, 20},
+        {20, 20, 0},
+        {20, 20, 20},
+        {20, 40, 20},
+        {20, 40, 0},
+        {20, 40, 20},
+        {0, 40, 20},
+        {0, 40, 0},
+        {0, 40, 20},
+        {0, 20, 20},
+        
+        {0, 20, 0},
+        {0, 40, 0},
+        {20, 40, 0},
+        {20, 20, 0},
+        {0, 20, 0}
+    };
+    
+    int[][] nPoints = new int[16][2];
+    
+    public void aRun(int i) {
+        float j = (float) i / 4;
+
+        float angle = (float) Math.toRadians(j); 
+        float[] position = {20, 20, -20};
+        float[][] xRotate = {
+            {1, 0, 0},
+            {0, (float) Math.cos(angle), (float) -Math.sin(angle)},
+            {0, (float) Math.sin(angle), (float) Math.cos(angle)}
+        };
+        float[][] yRotate = {
+            {(float) Math.cos(angle), 0, (float) Math.sin(angle)},
+            {0, 1, 0},
+            {(float) -Math.sin(angle), 0, (float) Math.cos(angle)}
+        };
+        float[][] zRotate = {
+            {(float) Math.cos(angle), (float) -Math.sin(angle), 0},
+            {(float) Math.sin(angle), (float) Math.cos(angle), 0},
+            {0, 0, 1}
+        };
+        
+        float[][] xyMatrix = matrixMatrixMultiply(xRotate, yRotate);
+        float[][] xyzRotate = matrixMatrixMultiply(xyMatrix, zRotate);
+        
+        for (int row = 0; row < ROWS; row++){
+            for (int col = 0; col < COLUMNS; col++){
+                grid[row][col] = EMPTY;
+            }
+        }
+        
+        int l = 0;
+        
+        for (float[] point : points) {
+            float[][] dPos = {matrixVectorMultiply(xyzRotate, point)};
+            dPos[0][1] += ROWS / 2;
+            dPos[0][0] += COLUMNS / 2;
+            pointMaker(dPos, POINT);
+            nPoints[l][0] = (int) dPos[0][0];
+            nPoints[l][1] = (int) dPos[0][1];
+            l++;
+        }
+        for (int k = 0; k < nPoints.length; k++) {
+            if (k < nPoints.length - 1) {
+                drawLine(nPoints[k][0], nPoints[k][1], nPoints[k + 1][0], nPoints[k + 1][1]);
+            }
+            else {
+                drawLine(nPoints[k][0], nPoints[k][1], nPoints[0][0], nPoints[0][1]);
+                System.out.println(nPoints[0][0]);
+            }
+        }
     }
 
     //called repeatedly.
@@ -199,21 +232,13 @@ public class SandLab
                 locationClicked(mouseLoc[0], mouseLoc[1], display.getTool(), j);
             }
             
-            if (j == 1000) {
+            if (j == 1440) {
                 j = 0;
             }
             j++;
-            float[] position = {1, 4, 3, 1};
-            float[][] pos2 = {{1, 5, 2, 3}, {1, 5, 3, 4}, {3, 2, 1, 6}, {1, 7, 8, 4}};
-            float[][] matrix = perspectiveMatrix(50, 80, 120, 10, 2);
-            float[] fMatrix = matrixMultiply(matrix, position);
-            float[] mat2 = matrixMultiply(pos2, position);
-            
-            System.out.print(Arrays.toString(mat2) + " ");
-            
-            System.out.println();
+            aRun(j);
         }
     }
 
         
-}x
+}
